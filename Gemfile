1b2012855832c213d82913ad4287155b0ea109e8
emile <emile@blueantcorp.com>
2023-05-07 17:26:09 +0400

    add gemspec
    Signed-off-by: emile <emile@blueantcorp.com>

--- Gemfile
+++ Gemfile
@@ -1,3 +1,4 @@
 source "https://rubygems.org"
+gem "rake", "~> 13.0"
 gem "jekyll"
 gem "sass"
---
+++ lib/jekyll/index/page.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative "page/version"
+
+module Jekyll
+  module Index
+    module Page
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
